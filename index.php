b05cfcb4d51c9409f98cfa7e089b3f03bb7dc63c
Carlos Contreras <kradvenko@outlook.com>
2018-04-19 13:46:16 -0600

    Edi 002

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="css/Mapa.css" />
+    <script src="js/jquery-3.3.1.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/nervo.js"></script>
+
+    <title>Acervo artístico de Amado Nervo</title>
+    <asp:ContentPlaceHolder ID="head" runat="server">
+    </asp:ContentPlaceHolder>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                
+            </div>
+            <div class="col-4">
+                Inicio de sesión
+            </div>
+            <div class="col-4">
+                
+            </div>
+        </div>
+    </div>    
+</body>
+</html>
