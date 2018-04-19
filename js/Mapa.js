ca6ed45098384a9a0cd79272089d0ff23e0601e2
Carlos Contreras <kradvenko@outlook.com>
2018-04-18 22:36:47 -0600

    Edit 001

---
+++ js/Mapa.js
@@ -0,0 +1,9 @@
+﻿function mostrar_elecciones(tipo) {
+    $("#divEleccionesContainer").html($("#" + tipo).html());
+}
+//Elecciones para presidentes municipales (Ayuntamientos)
+function verEleccionesAyuntamiento(anio) {
+	$.ajax({url: "php/obtenerEleccionesAyuntamiento.php", async: false, type: "POST", data: {anio: anio}, success: function(res, statusText, jqXHR) {
+        $("#divMapa").html(res);
+	}});
+}
